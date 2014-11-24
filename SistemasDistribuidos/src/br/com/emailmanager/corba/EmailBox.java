9da87129513999cdbf13614bb746b01fb96e0acd
heloisakopsch <heloisa.kopsch@WS534.cetil.com.br>
2014-11-24 11:16:41 -0200

    Criada estrutura do Corba

---
+++ SistemasDistribuidos/src/br/com/emailmanager/corba/EmailBox.java
@@ -0,0 +1,51 @@
+package br.com.emailmanager.corba;
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
+import EmailBoxServer.Email_Box_Server;
+import EmailBoxServer.Email_Box_ServerHelper;
+
+public class EmailBox {
+
+	public static void main(String[] args) {
+		 try{
+		      // Cria e inicializa o ORB
+		      ORB orb = ORB.init(args, null);
+
+		      // Cria a implementação e registra no ORB
+
+		      // Ativa o POA
+		      POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+		      rootpoa.the_POAManager().activate();
+
+		      // Pega a referência do servidor
+		      org.omg.CORBA.Object ref = rootpoa.servant_to_reference(new EmailBoxPOA());
+		      Email_Box_Server href = Email_Box_ServerHelper.narrow(ref);
+			  
+		      // Obtém uma referência para o servidor de nomes
+		      org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+		      NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+		      // Registra o servidor no servico de nomes
+		      String name = "Email_Box_Server";
+		      NameComponent path[] = ncRef.to_name( name );
+		      ncRef.rebind(path, href);
+
+		      System.out.println("Servidor aguardando requisicoes ....");
+
+		      // Aguarda chamadas dos clientes
+		      orb.run();
+		    } catch (Exception e) {
+		        System.err.println("ERRO: " + e);
+		        e.printStackTrace(System.out);
+		    }
+		    System.out.println("Encerrando o Servidor.");
+
+	}	
+
+}
---
+++ SistemasDistribuidos/src/br/com/emailmanager/corba/EmailBoxPOA.java
@@ -0,0 +1,19 @@
+package br.com.emailmanager.corba;
+
+import EmailBoxServer.Email_Box_ServerPOA;
+
+public class EmailBoxPOA extends Email_Box_ServerPOA {
+
+	@Override
+	public void salvarEmail() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String consultarEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
